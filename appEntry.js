d3c1bc26d3a5780a73b35aef2d34b01333c308e2
ztercel <ztercel@163.com>
2018-12-30 23:08:38 +0800

    1. 优化网页表单 2. 完成所有核心功能

--- appEntry.js
+++ appEntry.js
@@ -1,6 +1,9 @@
 const Koa    = require('koa');
 const fs     = require('fs');
+const Brand  = require('./brand');
 const Router = require('koa-router');
+const logger = require("koa-logger");
+const parser = require('koa-bodyparser');
 
 const router    = Router();
 router.get('/', async(ctx, next) => {
@@ -8,33 +11,50 @@ router.get('/', async(ctx, next) => {
     ctx.response.body = fs.createReadStream('./resource/page.html');
 });
 router.post('/', async(ctx, next) => {
-   ctx.response.body = 'good';
+    await next();
 
-   console.log('goodddddd');
+    let {category, categoryName, name} = ctx.request.body;
+    let brand = new Brand();
+
+    let data = await brand.inquire(name, category, categoryName);
+    ctx.response.status = 200;
+    ctx.response.body = data;
+    console.log(data);
 });
 
 const app    = new Koa();
+app.use(logger());
+app.use(parser());
 app.use(router.routes());
 app.listen(3000);
-
-console.log('service is running...');
+console.log('service(port: 3000) is running...');
 
 /*
-const Inquirer  = require('./Inquirer/inquirer');
-const Generator = require('./Generator/generator');
-
-const seeds = '卓美';//美宫蝶云万如双致简子伊天尹星秋梦兰长欧枫千语';
- type = 20;  // 家具
-const inquirer  = new Inquirer();
-const generator = new Generator();
-inquirer.inquire(generator.generate(seeds), type);
+const name = '福民', type = 21;
+let brand = new Brand();
+async function test() {
+   console.log(await brand.inquire(name, type, "type"));
+};
+test();
 */
 
+//const Generator = require('./Generator/generator');
+/*
+const seeds = '卓美美宫蝶云万如双致简子伊天尹星秋梦兰长欧枫千语';
+ type = 21;  // 家具
+const brand  = new Brand();
+//const generator = new Generator();
+brand.batchInquire(['卓美'], type); */
 
 
 
 
 
+
+/*
+var items = [ {name: '没没', items: [{company: 'ddd', no:'1212', status: '注册'}] },
+{name: '声所', items: [{company: 'ddd', no:'1212', status: '未注册'}, {company: 'cccc', no:'1212', status: '注册'}] }];
+*/
 
 
 
